be7f125fbd9d3112a9bc45f68ae5cf69806c9a4c
Ariana1825 <apreyes18@outlook.com>
2026-05-22 18:55:16 -0500

    actualizando

--- src/pages/Generos/GenerosDetalle.tsx
+++ src/pages/Generos/GenerosDetalle.tsx
@@ -25,9 +25,9 @@ import Header from '../game/Header';
         <div className="bg-[#1a1a1a] p-8 font-sans justify-centers shadow-black">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-[70%] "style={{ margin: '2.5rem auto' }}>
             {data.map((juego) => (
-              <div key={juego.id} style={{ padding: '2.5rem 1.5rem' }} className="w-[100%] rounded-lg flex flex-col
+              <div onClick={() => navigate(`/videojuegos/${juego.id}`)}  key={juego.id} style={{ padding: '2.5rem 1.5rem' }} className="w-[100%] rounded-lg flex flex-col
            items-center gap-2 hover:scale-[1.02] transition-transform">
-            <img onClick={() => navigate(`/videojuegos/${juego.id}`)} src={juego.imagen_url} alt={juego.nombre} className="w-[55%] mb-4 shadow-2xl rounded-lg" />
+            <img  src={juego.imagen_url} alt={juego.nombre} className="w-[55%] mb-4 shadow-2xl rounded-lg" />
             <h1 className="text-white font-bold text-lg mb-2">{juego.nombre}</h1>
             <p className="text-gray-400 text-md leading-relaxed">{juego.resena}</p>
           </div>
