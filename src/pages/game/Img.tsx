bd394a7ede044eddc92bb66c989907f33f86d974
Ariana1825 <apreyes18@outlook.com>
2026-05-22 13:24:06 -0500

    Mejorando rutas de imagenes

--- src/pages/game/Img.tsx
+++ src/pages/game/Img.tsx
@@ -1,11 +1,11 @@
 const Img = () => {
   return (
     <div className='flex items-center imagenes'>
-          <img src='/public/icons/img.jpg' className='imagen1 w-[500px] shadow-lg'/>
-          <img src='/public/icons/img1.jpg' className='imagen2 w-[500px] shadow-lg'/>
-          <img src='/public/icons/img2.jpg' className='imagen3 w-[500px] shadow-lg'/>
-          <img src='/public/icons/img3.jpg' className='imagen4 w-[500px] shadow-lg'/>
-          <img src='/public/icons/img4.jpg' className='imagen5 w-[500px] shadow-lg'/>
+          <img src='/icons/img.jpg' className='imagen1 w-[500px] shadow-lg'/>
+          <img src='/icons/img1.jpg' className='imagen2 w-[500px] shadow-lg'/>
+          <img src='/icons/img2.jpg' className='imagen3 w-[500px] shadow-lg'/>
+          <img src='/icons/img3.jpg' className='imagen4 w-[500px] shadow-lg'/>
+          <img src='/icons/img4.jpg' className='imagen5 w-[500px] shadow-lg'/>
         </div>
   )
 }
