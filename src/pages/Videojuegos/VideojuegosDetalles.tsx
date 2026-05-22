6313571e3a9f16ea245d1b30c5152d0a7e2e4049
Ariana1825 <apreyes18@outlook.com>
2026-05-22 18:10:48 -0500

    Actualizando diseño de videojuego por id

--- src/pages/Videojuegos/VideojuegosDetalles.tsx
+++ src/pages/Videojuegos/VideojuegosDetalles.tsx
@@ -1,5 +1,6 @@
 import { useNavigate, useParams } from 'react-router-dom';
 import { useVideojuegos } from './useVideojuegos';
+import Header from '../game/Header';
 
 const VideojuegosDetalles = () => {
     const { id } = useParams()
@@ -8,11 +9,22 @@ const { videojuegoActual, cargando, error } = useVideojuegos(id!)
 if (cargando) return <p>Cargando...</p>
 if (error) return <p>{error}</p>
   return (
+
     <div>
-    <h1 className="text-white">{videojuegoActual?.nombre}</h1>
-    <img src={videojuegoActual?.imagen_url} alt={videojuegoActual?.nombre} />
-    <p>{videojuegoActual?.resena}</p>
-  </div>
+    <Header imagen="img1.jpg" titulo="Videojuego" parrafo="Dale un vistazo a los demás videojuegos!"/>  
+     <div className="bg-[#1a1a1a] p-8 font-sans justify-centers shadow-black">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-[70%] "style={{ margin: '2.5rem auto' }}>
+            <div className="w-[40%] rounded-lg flex flex-col">
+              <h1 className="text-white">{videojuegoActual?.nombre}</h1>
+              <img src={videojuegoActual?.imagen_url} alt={videojuegoActual?.nombre} />
+            </div>
+            <div className=" w-[100%] rounded-lg flex flex-col">
+              <p>{videojuegoActual?.resena}</p>
+            </div>
+      
+      </div>
+    </div>
+    </div>
   )
 }
 
