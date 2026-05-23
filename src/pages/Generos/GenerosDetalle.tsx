ac799721d2aa598512f155301e87010912b5cdbc
Ariana1825 <apreyes18@outlook.com>
2026-05-22 22:27:26 -0500

    actualizando

--- src/pages/Generos/GenerosDetalle.tsx
+++ src/pages/Generos/GenerosDetalle.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from '@tanstack/react-query'
 import { fetchVideojuegosGenero } from '../../services/videojuegos.services'
-import { useNavigate, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { Link } from 'react-router-dom'
 import Header from '../game/Header';
  const GenerosDetalle = () => {
 
@@ -10,7 +11,6 @@ import Header from '../game/Header';
     queryFn: () => fetchVideojuegosGenero(id),
     });
 
-  const navigate = useNavigate();
   if (isLoading) {
     return <div>Cargando...</div>;
   }
@@ -21,21 +21,24 @@ import Header from '../game/Header';
 
   return (
     <div>
-        <Header imagen="img1.jpg" titulo="Género" parrafo="Dale un vistazo a los demás géneros!"/>
-        <div className="bg-[#1a1a1a] p-8 font-sans justify-centers shadow-black">
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-[70%] "style={{ margin: '2.5rem auto' }}>
-            {data.map((juego) => (
-              <div onClick={() => {console.log('click en juego', id, juego.id);
-                navigate(`/generos/${id}/videojuegos/${juego.id}`)}}  key={juego.id} style={{ padding: '2.5rem 1.5rem' }} className="w-[100%] rounded-lg flex flex-col
-           items-center gap-2 hover:scale-[1.02] transition-transform">
-            <img  src={juego.imagen_url} alt={juego.nombre} className="w-[55%] mb-4 shadow-2xl rounded-lg" />
-            <h1 className="text-white font-bold text-lg mb-2">{juego.nombre}</h1>
-            <p className="text-gray-400 text-md leading-relaxed">{juego.resena}</p>
-          </div>
-            ))}
-            </div>
-      </div>
+  <Header imagen="img1.jpg" titulo="Género" parrafo="Dale un vistazo a los demás géneros!"/>
+  <div className="bg-[#1a1a1a] p-8 font-sans shadow-black">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 w-[70%]" style={{ margin: '2.5rem auto' }}>
+      {data.map((juego) => (
+        <Link
+          to={`/generos/${id}/videojuegos/${juego.id}`}
+          key={juego.id}
+          style={{ padding: '2.5rem 1.5rem' }}
+          className="w-[100%] rounded-lg flex flex-col items-center gap-2 hover:scale-[1.02] transition-transform no-underline"
+        >
+          <img src={juego.imagen_url} alt={juego.nombre} className="w-[55%] mb-4 shadow-2xl rounded-lg" />
+          <h1 className="text-white font-bold text-lg mb-2">{juego.nombre}</h1>
+          <p className="text-gray-400 text-md leading-relaxed">{juego.resena}</p>
+        </Link>
+      ))}
     </div>
+  </div>
+</div>
   );
 };
 export default GenerosDetalle
