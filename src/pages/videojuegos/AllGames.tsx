877239cabaeb73ca56bbc8b54ba71399d4cc6e95
ArianaEyes <61152185@mail.isil.pe>
2026-06-11 09:15:55 -0500

    Rediseñando

--- src/pages/videojuegos/AllGames.tsx
+++ src/pages/videojuegos/AllGames.tsx
@@ -45,14 +45,16 @@ console.log(videojuegos.map(v => v.id));
 
                         {videojuegos.map(itemVideojuegos => (
                             <figure
-                            key={itemVideojuegos.id}
-                            className="group relative overflow-hidden rounded-md"
-                            >
-                         <img
-                           src={itemVideojuegos.imagen_url}
-                           alt={itemVideojuegos.nombre}
-                           className="w-full h-70 rounded-md object-cover transition-transform duration-300 group-hover:scale-105"
-                         />
+  key={itemVideojuegos.id}
+  className="group relative overflow-hidden rounded-md"
+>
+  <div className="w-full h-56 overflow-hidden rounded-md">
+    <img
+      src={itemVideojuegos.imagen_url}
+      alt={itemVideojuegos.nombre}
+      className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+    />
+  </div>
 
                     <div
                          className="absolute inset-0 bg-black/60
