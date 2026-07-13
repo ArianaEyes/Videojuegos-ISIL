c76b484abf64792125798ecd677746728ef6e926
ArianaEyes <61152185@mail.isil.pe>
2026-07-13 17:18:28 -0500

    Mejorando diseño palte 4

--- src/pages/favoritos/index.tsx
+++ src/pages/favoritos/index.tsx
@@ -5,7 +5,7 @@ import Header from '../../components/Header'
 const Favoritos = () => {
   return (
     <div>
-      <Header imagen="img1.jpg" titulo="Favoritos" 
+      <Header imagen="img9.jpg" titulo="Favoritos" 
                 parrafo="Tus videojuegos favoritos"/>
         <section>
             <div className='max-w-7xl mx-auto px-3 py-20'>
